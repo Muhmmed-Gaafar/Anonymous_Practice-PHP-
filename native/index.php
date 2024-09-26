85a5f448a2e44122d9f51923569bbe2412a48ffa
Muhmmed-Gaafar <muhmmedgaafar@gmail.com>
2024-09-26 17:07:04 +0300

    upload

--- native/index.php
+++ native/index.php
@@ -1,10 +1,11 @@
 <?php
 echo $_REQUEST['test'];
-
+echo $_SERVER['SERVER_SOFTWARE'];
+ echo$_SERVER['SERVER_PROTOCOL'];
 ?>
-<form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?> " >
+<form method="post" action=" upload.php " enctype="multipart/form-data" >
     <label>
-        <input type="text" name="test" />
+        <input type="file" name="my_file" />
     </label>
     <input type="submit" value="send" />
 </form>
