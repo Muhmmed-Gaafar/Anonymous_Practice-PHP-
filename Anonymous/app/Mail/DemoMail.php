3b9ffa4fc9306f57cfa6497e590bc70d7d27be70
Muhmmed-Gaafar <muhmmedgaafar@gmail.com>
2024-10-08 22:28:01 +0300

    update

---
+++ Anonymous/app/Mail/DemoMail.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class DemoMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function build()
+    {
+        return $this->view('emails.demo')
+            ->with('data', $this->data);
+    }
+}
+
--- Anonymous/routes/web.php
+++ Anonymous/routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\ExampleController;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
+use App\Mail\DemoMail;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,4 +17,9 @@ Route::get('/index','ExampleController@index')->name('index');
 Route::controller(ExampleController::class)->group(function () {
     Route::get('/test', 'test');
 });
+
+
+
+
+
 
